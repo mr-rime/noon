819f3727addaba4748e969a23cec6e498a7b4f84
rimex3 <faresahmed51857@gmail.com>
2025-05-21 02:04:21 +0300

    refactor: remove old App component and styles, integrate TanStack Router and Apollo Client
    - Deleted App.css and App.tsx files to clean up the project structure.
    - Updated index.css to use Tailwind CSS for styling.
    - Modified main.tsx to set up TanStack Router and Apollo Client for routing and data fetching.
    - Added routeTree generation for routing management.
    - Created root and index routes with basic components for rendering.
    - Introduced Apollo Client configuration for GraphQL communication.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient()
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>,
+  )
+}
