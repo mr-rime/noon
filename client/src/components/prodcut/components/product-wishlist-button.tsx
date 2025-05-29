eecf91338e5fc1da4e659a5a65e8d0583f613966
rimex3 <faresahmed51857@gmail.com>
2025-05-29 23:19:14 +0300

    feat: update hover effect for cart and wishlist buttons to improve user experience

--- client/src/components/prodcut/components/product-wishlist-button.tsx
+++ client/src/components/prodcut/components/product-wishlist-button.tsx
@@ -2,7 +2,7 @@ import { product_icons } from "../constants/icons";
 
 export function ProductWishlistButton() {
     return (
-        <button className="w-[28px] h-[28px] bg-white hover:bg-white/50 transition-colors shadow-md rounded-[6px] flex items-center justify-center cursor-pointer">
+        <button className="w-[28px] h-[28px] bg-white hover:bg-white/90 transition-colors shadow-md rounded-[6px] flex items-center justify-center cursor-pointer">
             {product_icons.wishlist2}
         </button>
     )
