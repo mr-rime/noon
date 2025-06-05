c1a13cf57451bbde550cb6fed0eccd8e9b125021
rimex3 <faresahmed51857@gmail.com>
2025-06-05 15:31:12 +0300

    feat: implement ProductPageTitle and ProductPageRates components for product details display

--- client/src/components/product-page/index.tsx
+++ client/src/components/product-page/index.tsx
@@ -1,10 +1,12 @@
 import { ProductPageGallery } from "./components/product-page-gallery";
 import { ProductPageImage } from "./components/product-page-image";
+import { ProductPageTitle } from "./components/product-page-title";
 
 export default function ProductPage() {
     return (
-        <div className="w-full flex items-center justify-start mt-20">
+        <div className="w-full flex items-start justify-start mt-20">
             <ProductPageImage gallery={<ProductPageGallery />} />
+            <ProductPageTitle />
         </div>
     )
 }
