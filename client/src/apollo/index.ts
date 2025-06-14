4d2531c207777712a040865a474702e3746b2372
rimex3 <faresahmed51857@gmail.com>
2025-06-15 00:20:12 +0300

    feat: update GraphQL endpoint and CORS configuration for production

--- client/src/apollo/index.ts
+++ client/src/apollo/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    uri: 'https://d8df-156-203-223-112.ngrok-free.app/graphql',
     cache: new InMemoryCache(),
     credentials: 'include'
 });
